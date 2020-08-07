c73c75bf938402d83e450873b9702c53e98f074a
hyeonminchoi5@gmail.com <hyeonminchoi5@gmail.com>
2020-08-07 10:07:56 +0900

    .

---
+++ OperatorOverloadingApp.cs/EventApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+class EventApp : Form
+{
+    public EventApp()
+    {
+        this.Click += new EventHandler(ClickEvent); //이벤트 처리기 등록
+    }
+
+    void ClickEvent(object sender, EventArgs e)
+    {
+        MessageBox.Show("Hello world");
+    }
+	public static void Main()
+	{
+        ApplicationException.Run(new EventApp());
+	}
+}
