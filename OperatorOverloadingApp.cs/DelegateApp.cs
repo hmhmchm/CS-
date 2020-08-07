d8fc277a7c111134440691a21468213c7467ef60
hyeonminchoi5@gmail.com <hyeonminchoi5@gmail.com>
2020-08-07 09:54:16 +0900

    .

---
+++ OperatorOverloadingApp.cs/DelegateApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+delegate void SampleDelegate(); // 델리게이트 정의
+class DelegateClass1
+{
+	public void DelegateMethod() //델리게이트할 메소드
+	{
+        Console.WriteLine("In the DelegateClass.DelegateMethod");
+	}
+
+    class DelegateApp
+    {
+        public static void Main()
+        {
+            DelegateClass obj = new DelegateClass();
+            SampleDelegate sd = new SampleDelegate(obj.DelegateMethod);
+            sd();  //invoke obj.DelegateMethod() indirectly.
+        }
+    }
+}
