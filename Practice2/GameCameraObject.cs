654be69eefd7477730b5643b7d50d110e4286f86
hyeonminchoi5@gmail.com <hyeonminchoi5@gmail.com>
2020-08-13 17:08:28 +0900

    c

---
+++ Practice2/GameCameraObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UItransform : MonoBehaviour
+{
+    [SerializeField] Transform _transform;
+    [SerializeField] Transform aTransform;
+    [SerializeField] Transform bTransform;
+    [SerializeField] Transform cTransform;
+    [SerializeField] Transform dTransform;
+
+    public void AButton()
+    {
+        _transform.position = aTransform.position;
+    }
+
+    public void BButton()
+    {
+        _transform.position = bTransform.position;
+    }
+
+    public void CButton()
+    {
+        _transform.position = cTransform.position;
+    }
+
+    public void DButton()
+    {
+        _transform.position = dTransform.position;
+    }
+}
